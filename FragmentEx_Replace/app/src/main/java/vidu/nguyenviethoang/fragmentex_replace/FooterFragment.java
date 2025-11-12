ab5ad266b20deaa4569c49741c6cca6d4d653bf9
Nguyễn Việt Hoàng <151610892+nguyenviethoang2005@users.noreply.github.com>
2025-11-12 16:22:37 +0700

    Thêm FragmentEx_Replace

---
+++ FragmentEx_Replace/app/src/main/java/vidu/nguyenviethoang/fragmentex_replace/FooterFragment.java
@@ -0,0 +1,66 @@
+package vidu.nguyenviethoang.fragmentex_replace;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class FooterFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_footer, container, false);
+
+        // Find views
+        Button b1 = v.findViewById(R.id.btn1);
+        Button b2 = v.findViewById(R.id.btn2);
+        Button b3 = v.findViewById(R.id.btn3);
+
+        //
+        FragmentManager fragmentManager = getParentFragmentManager();
+        // Event
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // code here
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_Content, new Fragment1())
+                        .commit();
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // code here
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_Content, new Fragment2())
+                        .commit();
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // code here
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_Content, new Fragment3())
+                        .commit();
+            }
+        });
+
+        return v;
+    }
+}
