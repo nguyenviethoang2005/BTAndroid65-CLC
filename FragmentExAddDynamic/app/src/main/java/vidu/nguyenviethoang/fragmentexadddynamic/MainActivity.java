294457fc50e7a3e796081890a2be185eeaad7ef8
Nguyễn Việt Hoàng <151610892+nguyenviethoang2005@users.noreply.github.com>
2025-11-12 16:09:09 +0700

    Thêm FragmentExAddDynamic

---
+++ FragmentExAddDynamic/app/src/main/java/vidu/nguyenviethoang/fragmentexadddynamic/MainActivity.java
@@ -0,0 +1,27 @@
+package vidu.nguyenviethoang.fragmentexadddynamic;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.frameContent, new ContentFragment())
+                .add(R.id.frameFooter, new FooterFragment())
+                .commit();
+    }
+}
